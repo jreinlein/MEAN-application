fc11aabceead27846ddbb21d2181a0cb5cbff9f8
jreinlein <jamesreinlein@gmail.com>
2015-10-29 21:25:22 -0400

    Start of search functionality

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,7 +1,12 @@
 // public/js/controllers/MainCtrl.js
 
-angular.module('MainCtrl', []).controller('MainController', function($scope) {
+angular.module('MainCtrl', []).controller('MainController', ['$scope', function($scope) {
 
-	$scope.tagline = 'To the moon and back!';
+	$scope.tagline = 'Search a certain #hashtag on Instagram!';
 
-});
+	$scope.searchedTerm = {};
+
+	$scope.update = function(givenTerm) {
+		$scope.searchedTerm = angular.copy(givenTerm);
+	};
+}]);
